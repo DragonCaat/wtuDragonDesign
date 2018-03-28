b26b7c66f988ec563c44daa84592b6a8006bea8f
DragonCaat <1446230222@qq.com>
2018-03-28 19:33:17 +0800

    mac-2018.3.28

--- app/src/main/java/com/dragon/wtudragondesign/application/DragonApplication.java
+++ app/src/main/java/com/dragon/wtudragondesign/application/DragonApplication.java
@@ -1,7 +1,11 @@
 package com.dragon.wtudragondesign.application;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.PackageManager;
 import android.support.multidex.MultiDexApplication;
+import android.support.v4.app.ActivityCompat;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.hyphenate.chat.EMClient;
@@ -25,7 +29,7 @@ public class DragonApplication extends MultiDexApplication {
         EMOptions options = new EMOptions();
        // 默认添加好友时，是不需要验证的，改成需要验证
         options.setAcceptInvitationAlways(true);
-       // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+       // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发          者自己处理附件消息的上传和下载
         options.setAutoTransferMessageAttachments(true);
        // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
         options.setAutoDownloadThumbnail(true);
